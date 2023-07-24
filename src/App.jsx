8d7403f11492401baf27a4c837bee10ab8939ac2
unknown <komauk1998@gmail.com>
2023-07-24 11:40:51 +0630

    chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Paths from './Routes/Paths'
+import Chatbot from './Pages/Chatbot'
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <>
     <Navbar/>
     <Paths/>
+    <Chatbot/>
     </>
   )
 }
